50775bd07e2d30b388db7ea19342a728b052f1b9
paulson05 <onyebuchiokofu@gmail.com>
2023-01-03 07:52:14 -0800

    first commit

---
+++ app/Http/Controllers/ProxyTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProxyType;
+use Illuminate\Http\Request;
+
+class ProxyTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProxyType  $proxyType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProxyType $proxyType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProxyType  $proxyType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProxyType $proxyType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProxyType  $proxyType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProxyType $proxyType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProxyType  $proxyType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProxyType $proxyType)
+    {
+        //
+    }
+}
