45282a1068cef0ef303e3abbb5d57c10bad8721a
paulson05 <onyebuchiokofu@gmail.com>
2023-01-05 02:01:16 -0800

    laravel breeze added for auth

--- database/migrations/2023_01_03_152632_create_admins_table.php
+++ database/migrations/2023_01_03_152632_create_admins_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->id();
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
